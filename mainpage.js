0da8b663b963d3d5c51bbcd10056918550aa7e25
Power <219028810+MusclySoftwareEngineer@users.noreply.github.com>
2025-12-01 20:56:20 +0000

    Dec 1

---
+++ mainpage.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById("dogface");
+const ctx = canvas.getContext("2d");
+
+
+// Set line width
+ctx.lineWidth = 5;
+
+// dog face outline
+ctx.beginPath();
+ctx.translate(250, 850);
+ctx.moveTo(280, 55);
+ctx.lineTo(10, -50);
+ctx.quadraticCurveTo(-30, -100, -30, -600);
+ctx.quadraticCurveTo( 90, -970, 150, -700);
+ctx.lineTo(280, -700);
+ctx.lineTo(500, -700);
+ctx.lineTo(860, -700);
+ctx.quadraticCurveTo(920, -970, 1040, -600);
+ctx.quadraticCurveTo(1050, -100, 1020, -60);
+ctx.lineTo(650, 80);
+
+  
+
+
+ctx.stroke();
+
